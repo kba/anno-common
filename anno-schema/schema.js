513666c6d665c9a880a5f6216e2722e1f2839c13
Konstantin Baierer <unixprog@gmail.com>
2017-03-19 22:47:08 +0100

    schema.js: better context

--- anno-schema/schema.js
+++ anno-schema/schema.js
@@ -2,12 +2,13 @@ const ajv = require('ajv')({
     allErrors: true
 });
 const config = require('@kba/anno-config').loadConfig({
-    // const PROP_HAS_COMMENT = 'http://purl.org/stuff/rev#hasReview'
-    PROP_HAS_COMMENT: 'ns:hasReview',
-    // const PROP_HAS_VERSION = 'http://purl.org/dcterms/hasVersion'
-    PROP_HAS_VERSION: 'ns:hasVersion',
-    // const PROP_VERSION_OF: 'http://purl.org/dcterms/versionOf'
-    PROP_VERSION_OF: 'ns:versionOf',
+    // PROP_HAS_REPLY: 'hasReply',
+    // PROP_REPLY_TO: 'replyTo',
+    // // PROP_HAS_COMMENT: 'ns:hasReview',
+    // PROP_HAS_VERSION: 'http://purl.org/dcterms/hasVersion',
+    // // PROP_HAS_VERSION: 'ns:hasVersion',
+    // PROP_VERSION_OF: 'http://purl.org/dcterms/versionOf',
+    // // PROP_VERSION_OF: 'ns:versionOf',
 })
 
 const schemaDef = require('./schema.json')
@@ -18,12 +19,53 @@ module.exports = {
     jsonldContext: [
         'http://www.w3.org/ns/anno.jsonld',
         {
-            'AnnotationReply': {
+            annox: {
+                '@id': 'https://github.com/kba/anno/context.jsonld',
+                '@type': "@id",
             },
-            'hasVersion': config.PROP_HAS_VERSION,
-            'versionOf': config.PROP_VERSION_OF,
-            'replyTo': config.PROP_REPLY_TO,
-        }
+            rdfs: {
+                '@id': 'http://www.w3.org/2000/01/rdf-schema#',
+                '@type': "@id",
+            },
+            sioc: {
+                '@id': 'http://rdfs.org/sioc/ns#',
+                '@type': "@id",
+            },
+            AnnotationReply: {
+                'rdfs:subClassOf': 'oa:Annotation',
+            },
+            hasReply: {
+                '@id': 'annox:hasReply',
+                '@type': "@id",
+                'rdfs:subClassOf': "sioc:has_reply",
+                'rdfs:domain': "oa:Annotation",
+                'rdfs:range': "annox:AnnotationComment",
+                'owl:inverseOf': {
+                    '@id': "hasReply"
+                }
+            },
+            replyTo: {
+                '@id': "annox:replyTo",
+                '@type': "@id",
+                'rdfs:subClassOf': "sioc:reply_to",
+                'rdfs:domain': "annox:AnnotationComment",
+                'rdfs:range': "oa:Annotation",
+                'owl:inverseOf': {
+                    '@id': "replyTo"
+                }
+            },
+            hasVersion: {
+                '@id': "http://purl.org/pav/hasVersion",
+                '@type': "'@id'"
+            },
+            versionOf: {
+                '@id': "ubhdanno:versionOf",
+                '@type': "'@id'",
+                'owl:inverseOf': {
+                    '@id': "hasVersion"
+                }
+            },
+        },
     ],
 }
 
