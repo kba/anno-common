cce545e8de677671083cf29a8016617e8a1ca97a
Konstantin Baierer <unixprog@gmail.com>
2017-03-27 01:46:51 +0200

    Moved prepublish script to /scripts for reuse

--- scripts/yml2json.js
+++ scripts/yml2json.js
@@ -2,11 +2,10 @@ const fs = require('fs')
 const yaml = require('js-yaml')
 
 function yaml2json(infile) {
-    infile = `${__dirname}/../${infile}.yml`
     const outfile = infile.replace('.yml', '.json')
     const obj = yaml.safeLoad(fs.readFileSync(infile, {encoding:'utf8'}))
     fs.writeFileSync(outfile, JSON.stringify(obj, null, 2), {encoding:'utf8'})
 }
-yaml2json('data-model')
-yaml2json('context')
-yaml2json('openapi')
+process.argv.slice(2).forEach(infile => {
+    yaml2json(infile)
+})
