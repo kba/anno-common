18af327bb455d2f348b91998f50111877b3eafbc
Konstantin Baierer <unixprog@gmail.com>
2017-04-01 11:39:16 +0200

    server: Allow CORS Authorization

--- anno-server/middleware/cors.js
+++ anno-server/middleware/cors.js
@@ -1,7 +1,7 @@
 module.exports = () => {
     return (req, resp, next) => {
         resp.header('Access-Control-Allow-Origin', '*')
-        resp.header('Access-Control-Allow-Headers', 'Content-Type, Prefer')
+        resp.header('Access-Control-Allow-Headers', 'Content-Type, Prefer, Authorization')
         resp.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, DELETE, PUT')
         resp.header('Access-Control-Allow-Origin', '*')
         resp.header('Access-Control-Expose-Headers', 'ETag, Allow, Vary, Link, Content-Type, Location, Content-Location, Prefer')
