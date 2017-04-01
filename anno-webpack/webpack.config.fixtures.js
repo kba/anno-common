9fb812ba3ad72bffff44042de46393e41fb4ba96
Konstantin Baierer <unixprog@gmail.com>
2017-04-02 01:52:25 +0200

    webpack: fix ajv issue, min/non-min files, sourcemaps

--- anno-webpack/webpack.config.fixtures.js
+++ anno-webpack/webpack.config.fixtures.js
@@ -4,7 +4,7 @@ const path = require('path');
 const config = {
     entry: 'json-loader!../anno-fixtures/index.json',
     output: {
-        path: path.resolve(__dirname, '..', 'dist'),
+        path: path.resolve(__dirname, '..', 'docs', 'dist'),
         filename: 'anno-fixtures.js',
         library: ['Anno', 'fixtures']
     },
--- anno-webpack/webpack.config.js
+++ anno-webpack/webpack.config.js
@@ -1,16 +1,26 @@
 const webpack = require('webpack');
 const path = require('path');
 
+// detect if webpack bundle is being processed in a production or development env
+let prodBuild = require('yargs').argv.p || false;
+
 const config = {
-    entry: './entry.js',
+    entry: {
+        core: './core.js',
+    },
     node: { fs: "empty" },
     output: {
-        path: path.resolve(__dirname, '..', 'dist'),
-        filename: 'anno.js',
-        library: 'Anno'
+        path: path.resolve(__dirname, '..', 'docs', 'dist'),
+        filename: `anno${prodBuild ? '.min' : ''}.js`,
+        library: 'Anno',
+        libraryTarget: 'umd'
     },
     resolve: {
         unsafeCache: true,
+        alias: {
+            'ajv': 'ajv/dist/ajv.min.js',
+            // 'nedb': 'nedb/browser-version/out/nedb.min.js',
+        }
     },
     module: {
         rules: [
@@ -25,7 +35,22 @@ const config = {
                 }
             }
         ]
-    }
+    },
+    devtool: prodBuild ? 'source-map' : 'eval-source-map',
+    // plugins: [
+    //     new webpack.optimize.CommonsChunkPlugin({
+    //         name: 'vendor',
+    //         minChunks: function (module) {
+    //             // this assumes your vendor imports exist in the node_modules directory
+    //             return module.context && module.context.indexOf('node_modules') !== -1;
+    //             // return module.context && (
+    //             //     module.context.indexOf('async') !== -1
+    //             //     || module.context.indexOf('ajv') !== -1
+    //             //     || module.context.indexOf('nedb') !== -1
+    //             //     || module.context.indexOf('localforage') !== -1
+    //         }
+    //     }),
+    // ]
 };
 
 module.exports = config;
