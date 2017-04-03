7cdf9322636cd95380f72a9d1e9482f58be1ce8c
Konstantin Baierer <unixprog@gmail.com>
2017-04-03 08:41:51 +0200

    acl: use errors.forbidden for unauthorized errors

--- anno-acl/acl.js
+++ anno-acl/acl.js
@@ -1,18 +1,22 @@
 const {RuleSet} = require('sift-rule')
+const {loadConfig,getLogger} = require('@kba/anno-config')
+const errors = require('@kba/anno-errors')
 
 class AnnoAcl {
 
     process(ctx, cb) {
+        const config = loadConfig()
         ctx.collection = (ctx.collection || 'default')
+        getLogger('acl').silly("Matching against rules:", ctx)
         const matchingRule = this.rules.first(ctx)
-        if (process.env.ANNO_DEBUG == 'true') {
+        if (config.LOGLEVEL !== '') {
             console.log(`Rule '${matchingRule}' matched ${JSON.stringify(ctx)}`)
         }
         this.reason = matchingRule.name
         if (matchingRule.tail)
             return cb(null, matchingRule.name)
         else
-            return cb(matchingRule.name)
+            return cb(errors.forbidden(matchingRule.name, ctx))
     }
 
 }
