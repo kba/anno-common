99f473259e94f42c203571fbe0decd353af4a6c8
Konstantin Baierer <unixprog@gmail.com>
2017-04-09 12:11:30 +0200

    server: log only jwt errors not due to missing token

--- anno-server/middleware/jsonwebtoken.js
+++ anno-server/middleware/jsonwebtoken.js
@@ -8,7 +8,8 @@ module.exports = () => {
     const jwt = expressJWT({secret: secret})
     return (req, resp, next) => {
         jwt(req, resp, (err) => {
-            if (err) console.log("JWT Error", err)
+            if (err && err.code !== 'credentials_required')
+                console.log("JWT Error", err)
             // XXX Note we don't pass any errors on so ACL can be handled by
             // anno-acl
             next()
