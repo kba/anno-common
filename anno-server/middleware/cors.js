961b6e39b0173e2521f2ba69d5fcda4e07ffbfab
Konstantin Baierer <unixprog@gmail.com>
2017-04-11 00:09:09 +0200

    cors middleware: echo Origin as Access-Control-Allow-Origin

--- anno-server/middleware/cors.js
+++ anno-server/middleware/cors.js
@@ -1,9 +1,9 @@
 module.exports = () => {
     return (req, resp, next) => {
-        resp.header('Access-Control-Allow-Origin', '*')
+        resp.header('Access-Control-Allow-Origin', req.get('Origin'))
         resp.header('Access-Control-Allow-Headers', 'Content-Type, Prefer, Authorization')
+        resp.header('Access-Control-Allow-Credentials', 'true')
         resp.header('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, DELETE, PUT')
-        resp.header('Access-Control-Allow-Origin', '*')
         resp.header('Access-Control-Expose-Headers', 'ETag, Allow, Vary, Link, Content-Type, Location, Content-Location, Prefer')
         return next()
     }
