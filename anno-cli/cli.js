ec20d2cf861ccb1cf18097793ba1a71ab07cb6dc
Konstantin Baierer <unixprog@gmail.com>
2017-04-16 16:10:57 +0200

    Improve CLI tools

--- anno-cli/cli.js
+++ anno-cli/cli.js
@@ -1,9 +1,56 @@
 #!/usr/bin/env node
 
+const getStdin = require('get-stdin')
+const path = require('path')
+const {
+    emptyAnnotation,
+} = require('@kba/anno-queries')
+
+const x = {}
+x.fileIdHandler = function fileIdHandler(id, argv) {
+    if (id.match(/^urn:/)) {
+        return id
+    }
+    if (id.match(/^[a-z]+:\/\//)) {
+        return id
+    } else if (id.match(/^\//)) {
+        return 'file://' + encodeURI(id)
+    } else {
+        return 'file://' + encodeURI(path.resolve(argv.cwd, id))
+    }
+}
+
+
 var argv = require('yargs')
     .commandDir('cmds')
     .demandCommand()
     .help()
     .alias('h', 'help')
     .usage("$0 <cmd> <cmd-args>")
+    .options({
+        cwd: {
+            type: 'string',
+            description: 'Current working directory',
+            default: process.cwd(),
+        },
+        // TODO
+        'id-handler': {
+            type: 'string',
+            description: 'Function translating non-URL IDs to URL',
+            default: 'fileIdHandler',
+            coerce(str) { return x[str] }
+        },
+        'annotation': {
+            type: 'string',
+            alias: ['a'],
+            description: 'Base annotation.',
+            default: JSON.stringify(emptyAnnotation.create()),
+        },
+        'dry-run': {
+            type: 'boolean',
+            alias: ['n'],
+            description: 'Output what would be passed to the store.',
+            default: false
+        },
+    })
     .argv;
--- anno-cli/cmds/create.js
+++ anno-cli/cmds/create.js
@@ -1,30 +1,87 @@
-exports.command = 'create <target> <value> [values..]'
+const getStdin = require('get-stdin')
+const {add} = require('@kba/anno-util')
+const path = require('path')
+const prune = require('object-prune')
+const {
+    simpleTagBody,
+    semanticTagBody,
+    mediaFragmentResource,
+} = require('@kba/anno-queries')
+
+exports.command = 'create'
 exports.desc = 'Annotate a target'
 exports.builder = {
+    cwd: {
+        type: 'string',
+        description: 'Current working directory',
+        default: process.cwd(),
+    },
     tag: {
+        type: 'string',
+        description: 'simple tags to assign',
+        array: true,
+    },
+    semtag: {
+        type: 'string',
+        description: 'semantic tags (URI) to assign',
+        array: true,
+    },
+    'target-id': {
+        type: 'string',
+        alias: ['target.id', 'ti'],
+        description: 'ID of the target',
+    },
+    'target-type': {
+        type: 'string',
+        alias: ['target.type', 'tt'],
+        description: "Type of the target",
+        default: null,
+    },
+    mediafragment: {
+        alias: ['mf'],
+        type: 'string',
+        description: 'Fragment selector',
+    },
+    stdin: {
+        alias: ['i'],
         type: 'boolean',
-        description: 'Treat values as tags',
-        default: false
+        description: 'Read base annotation from STDIN',
     }
 }
-exports.handler = function (argv) {
-    argv.values.unshift(argv.value)
-    delete argv.value
 
-    const store = require('@kba/anno-store').load(module)
-    store.init(err => {
-        if (err) return console.error(err)
-        const values = argv.values.map(value => {
-            const anno = {target: argv.target, body: {type:[], value: value}}
-            if (argv.tag)
-                anno.body.type.push('oa:Tag')
-            else
-                anno.body.type.push('oa:TextualBody')
-            return anno
-        })
-        store.create(values, (err, annoSaved) => {
+exports.handler = function (argv) {
+
+    if (!argv.tag) argv.tag = []
+    if (!argv.semtag) argv.semtag = []
+
+    var target = argv.target
+    if (argv.mediafragment) {
+        target = Object.assign(mediaFragmentResource.create({value: argv.mediafragment}), target)
+    }
+    if (target.id) {
+        target.id = argv.idHandler(target.id, argv)
+    }
+
+    new Promise((resolve, reject) => {
+        if (argv.stdin) getStdin().then(resolve)
+        else resolve(argv.annotation)
+    }).then(annoJSON => {
+        const anno = JSON.parse(annoJSON)
+        argv.tag.map(value => add(anno, 'body', simpleTagBody.create({value})))
+        argv.semtag.map(id => add(anno, 'body', semanticTagBody.create({id})))
+        add(anno, 'target', target)
+        prune(anno)
+        if (argv.dryRun) {
+            console.log(JSON.stringify(anno, null, 2))
+            process.exit()
+        }
+        const store = require('@kba/anno-store').load(module)
+        store.init(err => {
             if (err) return console.error(err)
-            console.log(annoSaved)
+            store.create(anno, (err, annoSaved) => {
+                if (err) return console.error(err.message)
+                // console.log(annoSaved)
+            })
         })
     })
 }
