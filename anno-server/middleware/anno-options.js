a4d9c3d5333b85f2b46ab645b8dd96e4dd822d7e
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-04-28 12:38:17 +0200

    scrape 'slug' option from header

--- anno-server/middleware/anno-options.js
+++ anno-server/middleware/anno-options.js
@@ -2,12 +2,20 @@ module.exports = () => optionsFromRequest
 
 function optionsFromRequest(req, resp, next) {
     const ret = {}
+    // boolean
     ;['skipVersions', 'skipReplies', 'metadataOnly'].forEach(option => {
         if (option in req.query) {
             ret[option] = !! req.query[option].match(/^(true|1)$/)
             delete req.query[option]
         }
     })
+
+    // Header
+    ;['slug'].forEach(hdrName => {
+        if (req.header(hdrName)) ret[hdrName] = req.header(hdrName)
+    })
+
+    // user set from jwt
     ;['user'].forEach(option => {
         if (option in req) ret[option] = req[option]
     })
