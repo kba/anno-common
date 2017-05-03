baeb283f9245212e88f11dd5c37447a70cb1e5cd
Konstantin Baierer <unixprog@gmail.com>
2017-05-04 01:09:22 +0200

    Revert "forceDelete, fix wipe"
    This reverts commit 1ef2c5aade6bffbff4def894921e6233de4c9b54.

--- anno-store-mongodb/store-mongodb.js
+++ anno-store-mongodb/store-mongodb.js
@@ -23,12 +23,15 @@ class MongodbStore extends Store {
 
     _wipe(options, cb) {
         this.db.drop(err => {
-            if (err) {
-                // if (err.message.match('ns not found'))  return this.disconnect(cb)
-                return cb(err)
-            }
-            this._disconnect({}, err => this._init({}, cb))
-            // return cb(err)
+            if (!err) return this.disconnect(cb)
+            if (err.message.match('ns not found'))  return this.disconnect(cb)
+            return cb(err)
+            // if (err) {
+            //     // if (err.message.match('ns not found'))  return this.disconnect(cb)
+            //     return cb(err)
+            // }
+            // this._disconnect({}, err => this._init({}, cb))
+            // // return cb(err)
         })
     }
 
