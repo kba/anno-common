34f9794582b85600dd91fc6669378cfdfce78886
Konstantin Baierer <unixprog@gmail.com>
2017-05-07 02:51:54 +0200

    :bulb: README for errors

---
+++ anno-errors/README.md
@@ -0,0 +1,25 @@
+# anno-errors
+
+> Shared errors for anno-*
+
+<!-- BEGIN-MARKDOWN-TOC -->
+* [`badSlug(slug)`](#badslugslug)
+* [`annotationNotFound(id)`](#annotationnotfoundid)
+* [`replyNotFound(id)`](#replynotfoundid)
+* [`revisionNotFound(id, rev)`](#revisionnotfoundid-rev)
+* [`readonlyValue(id, field, valueBefore, value)`](#readonlyvalueid-field-valuebefore-value)
+* [`invalidAnnotation(anno, errors)`](#invalidannotationanno-errors)
+* [`forbidden(reason, ctx)`](#forbiddenreason-ctx)
+
+<!-- END-MARKDOWN-TOC -->
+
+<!-- BEGIN-RENDER ./errors.js -->
+## `badSlug(slug)`
+## `annotationNotFound(id)`
+## `replyNotFound(id)`
+## `revisionNotFound(id, rev)`
+## `readonlyValue(id, field, valueBefore, value)`
+## `invalidAnnotation(anno, errors)`
+## `forbidden(reason, ctx)`
+
+<!-- END-RENDER -->
--- anno-errors/errors.js
+++ anno-errors/errors.js
@@ -1,35 +1,53 @@
 module.exports = {
 
+    /*
+     * ## `badSlug(slug)`
+     */
     badSlug(slug) {
         const err = new Error(`Slug Bad: ${JSON.stringify(slug)}`)
         err.code = 400
         return err
     },
 
+    /*
+     * ## `annotationNotFound(id)`
+     */
     annotationNotFound(id) {
         const err = new Error(`Annotation not found in store: ${JSON.stringify(id)}`)
         err.code = 404
         return err
     },
 
+    /*
+     * ## `replyNotFound(id)`
+     */
     replyNotFound(id) {
         const err = new Error(`Reply not found in store: ${JSON.stringify(id)}`)
         err.code = 404
         return err
     },
 
+    /*
+     * ## `revisionNotFound(id, rev)`
+     */
     revisionNotFound(id, rev) {
         const err = new Error(`No revision '${rev}' for annotation '${id}'`)
         err.code = 404
         return err
     },
 
+    /*
+     * ## `readonlyValue(id, field, valueBefore, value)`
+     */
     readonlyValue(id, field, valueBefore, value) {
         const err = new Error(`Client must not change the '${field}' of annotation '${id}' from \n${JSON.stringify(valueBefore)} to ${JSON.stringify(value)}`)
         err.code = 409
         return err
     },
 
+    /*
+     * ## `invalidAnnotation(anno, errors)`
+     */
     invalidAnnotation(anno, errors) {
         const err = new Error(`Annotation is invalid: ` + JSON.stringify({anno, errors}, null, 2))
         err.validationErrors = errors
@@ -37,6 +55,9 @@ module.exports = {
         return err
     },
 
+    /**
+     * ## `forbidden(reason, ctx)`
+     */
     forbidden(reason, ctx) {
         const err = new Error(`ACL forbids it: ${reason}`)
         err.code = 403
