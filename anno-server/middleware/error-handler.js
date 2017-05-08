f4d7010f1fe9e3eaab1cda39d24ea1352f35610d
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-05-08 17:14:24 +0200

    error-middleware: emit http status code only where sensible

--- anno-server/middleware/error-handler.js
+++ anno-server/middleware/error-handler.js
@@ -1,6 +1,6 @@
 module.exports = () => {
     return (err, req, resp, next) => {
-        if (err && err.code) {
+        if (err && err.code && err.code < 600) {
             resp.status(err.code)
             resp.send(err.message) 
         } else if (err) {
