11b452185f435ff408c4a63030e8e8b585d0630c
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-05-10 11:10:36 +0200

    Upgrade JSON body-parser limit to 1 MiB

--- anno-server/server.js
+++ anno-server/server.js
@@ -16,7 +16,7 @@ function start(app, cb) {
     app.set('view engine', 'pug')
 
     app.use(require('body-parser').urlencoded({ extended: true }));
-    app.use(require('body-parser').json({type: '*/*'}))
+    app.use(require('body-parser').json({type: '*/*', limit: 1 * 1024 * 1024 }))
 
     app.use(require('cookie-parser')());
     app.use(require('express-session')({
