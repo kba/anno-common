237dda19e511a11e8dcd7a6a1b0bfae1b90d66dc
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-05-19 17:10:13 +0200

    anno-server: pass neither collectino or metadata (not in token)

--- anno-server/middleware/anno-options.js
+++ anno-server/middleware/anno-options.js
@@ -3,7 +3,7 @@ module.exports = () => optionsFromRequest
 function optionsFromRequest(req, resp, next) {
     const ret = {}
 
-    // boolean
+    // boolean values
     ;['skipVersions', 'skipReplies', 'metadataOnly'].forEach(option => {
         if (option in req.query) {
             ret[option] = !! req.query[option].match(/^(true|1)$/)
@@ -11,33 +11,13 @@ function optionsFromRequest(req, resp, next) {
         }
     })
 
-    // collection
-    ;['collection'].forEach(option => {
-        if (option in req.query) {
-            ret[option] = req.query[option]
-            delete req.query[option]
-        }
-    })
-
-
-    // context metadata
-    const metadata = {}
-    for (let option in req.query) {
-        if (option.indexOf('metadata.') === 0) {
-            metadata[option.replace('metadata.', '')] = req.query[option]
-            delete req.query[option]
-        }
-    }
-    if (Object.keys(metadata).length)
-        ret.metadata = metadata
-
     // Header
     ;['slug'].forEach(hdrName => {
         if (req.header(hdrName)) ret[hdrName] = req.header(hdrName)
     })
 
     // user set from jwt
-    ;['user'].forEach(option => {
+    ;['user', 'service'].forEach(option => {
         if (option in req) ret[option] = req[option]
     })
     console.log("Options scraped", ret)
