056e6de7f73026ea1918895a7e5e18ec24db2f2a
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-05-26 11:50:38 +0200

    :package: sift-rule 0.1.0 -> 0.1.1

--- anno-plugins/acl.js
+++ anno-plugins/acl.js
@@ -18,11 +18,20 @@ class AnnoAcl {
         const config = envyConf('ANNO')
         const log = envyLog('ANNO', 'acl')
         ctx.collection = (ctx.collection || 'default')
+        // process.env.SIFT_RULE_DEBUG = 'true'
         // log.silly("Matching against rules:", ctx)
+        // try {
+        //     log.silly(`Matching against rules:` + JSON.stringify({
+        //         'ctx.user': ctx.user,
+        //         'ctx.anno': ctx.anno,
+        //         'keys(ctx)': Object.keys(ctx)
+        //     }, null, 2))
+        // } catch (err) {}
+        process.env.SIFT_RULE_DEBUG = 'true'
         const matchingRule = this.rules.first(ctx) || FALLBACK_RULE
         // if (config.LOGLEVEL !== '') {
         //     // log.silly(`Rule '${matchingRule}' matched ${JSON.stringify(ctx).substring(0,100)}`)
-        //     log.silly(`Rule '${matchingRule}' matched`)
+        //    log.silly(`Rule '${matchingRule}' matched`)
         // }
         this.reason = matchingRule.name
         if (matchingRule.tail)
