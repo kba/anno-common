96f005114a789a3e72c167428b1ebf430cd590e7
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-09-26 17:16:25 +0200

    more intricate dev-server

--- anno-server/dev-server.sh
+++ anno-server/dev-server.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 lerna bootstrap
+ANNO_LOGLEVEL="silly" \
 ANNO_SERVER_AUTH="plain" \
 ANNO_STORE_FILE="/tmp/annotest.nedb" \
-ANNO_ENABLE_JWT_AUTH="" \
+ANNO_ENABLE_JWT_AUTH="true" \
+ANNO_MIDDLEWARE_PLUGINS='@kba/anno-plugins:PreCollectionStatic' \
+ANNO_COLLECTION_DATA='{"default":{"secret":"not really secret"}}' \
+ANNO_USER_DATA='{"john":{"public":{"displayName": "T.U.F.K.A. John"}}}' \
+ANNO_STORE_HOOKS_PRE="@kba/anno-plugins:PreUserStatic,@kba/anno-plugins:CreatorInjectorStatic,@kba/anno-plugins:PreAclStatic" \
 node server.js
