33e18f9ce970e2351cdb73af505ccf2227efef38
Konstantin Baierer <unixprog@gmail.com>
2017-10-23 14:33:00 +0200

    hook up importDoi to acl rule setup

--- anno-auth-server/dev-server.sh
+++ anno-auth-server/dev-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-lerna bootstrap
+lerna bootstrap --hoist
 ANNO_AUTH_BACKEND="plain" \
 ANNO_COLLECTION_FILE="$PWD/../../config/collections.yml" \
 node auth-server.js
--- anno-server/dev-server.sh
+++ anno-server/dev-server.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-lerna bootstrap
-# export ANNO_LOGLEVEL="silly"
-export ANNO_LOGLEVEL="debug"
+lerna bootstrap --hoist
+export ANNO_LOGLEVEL="silly"
+# export ANNO_LOGLEVEL="debug"
 export ANNO_STORE_FILE="/tmp/annotest.nedb"
 export ANNO_ENABLE_JWT_AUTH="true"
 export ANNO_MIDDLEWARE_PLUGINS='@kba/anno-plugins:PreCollectionStatic'
 export ANNO_STORE_HOOKS_PRE="
   @kba/anno-plugins:PreUserStatic,
+  @kba/anno-plugins:PreAclStatic,
   @kba/anno-plugins:CreatorInjectorStatic
   "
-export ANNO_USER_DATA='{"john":{"public":{"displayName": "T.U.F.K.A. John"}}}'
+# export ANNO_USER_DATA='{"john":{"public":{"displayName": "T.U.F.K.A. John"}}}'
 export ANNO_STORE_HOOKS_POST=""
 export ANNO_COLLECTION_DATA='{
   "default": {
-    "secret":"not really secret",
+    "secret":"123",
     "doiTemplate": "10.5072/foo.{{ fullid }}",
     "heiperEndpoint": "http://localhost:8989/dummy/dummy"
   }
